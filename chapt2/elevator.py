0434e53dd962d310e1e395691d3ee0c81083c5ed
Chris <c.lamothe25@gmail.com>
2018-08-03 22:22:20 -0400

    Chapters 1 & 2 exercises of py4e. Text, inputs, operators....

---
+++ chapt2/elevator.py
@@ -0,0 +1,3 @@
+ans = input("What floor number? ")
+usfl = int(ans) + 1
+print("The US floor is", usfl)
---
+++ chapt2/ex_05.py
@@ -0,0 +1,2 @@
+cel = input("What is the temperature in Celsius? ")
+print("The temperature in Fahrenheit is :", int(cel) * 9 / 5 + 32)
