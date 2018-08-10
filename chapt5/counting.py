5cbf0df70d75318bce160bbfcde57606a4b66f90
Chris <c.lamothe25@gmail.com>
2018-08-09 21:42:11 -0400

    More and more loops

--- chapt5/counting.py
+++ chapt5/counting.py
@@ -1,6 +1,25 @@
+# counting
 zork = 0
 print("Before: ", zork)
 for thing in [9, 41, 12, 3, 74, 15]:
     zork += 1
     print(zork, thing)
 print("After: ", zork)
+
+# summing
+zork = 0
+print("Before:", zork)
+for thing in [9, 41, 12, 3, 74, 15]:
+    zork = zork + thing
+    print(zork, thing)
+print("After:", zork)
+
+# Average
+count = 0
+sum = 0
+print("Before:", count, sum)
+for value in [9, 41, 12, 3, 74, 15]:
+    count += 1
+    sum = sum + value
+    print(count, sum, value)
+print("After:", count, sum, sum / count)
