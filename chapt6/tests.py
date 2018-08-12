7bc6213bcd60e8018d14ea69499dcce293010496
Chris <c.lamothe25@gmail.com>
2018-08-11 21:37:41 -0400

    Starting chapter 6, strings

---
+++ chapt6/tests.py
@@ -0,0 +1,51 @@
+# zot = 'abc'
+# print(zot[5])  # traceback error
+#
+# fruit = 'banana'
+# print(len(fruit))  # len is a buuilt in function
+
+# fruit = 'banana'
+# index = 0
+# while index < len(fruit):
+#     letter = fruit[index]
+#     print(index, letter)
+#     index += 1
+
+#  prefer for loop
+# fruit = 'banana'
+# for letter in fruit:
+#     print(letter)
+
+# loop and count
+# word = 'banana'
+# count = 0
+# for letter in word:  # letter is the iteration variable
+#     if letter == 'a':
+#         count += 1
+# print(count)
+
+# s = 'Monty Python'
+# print(s[0:4])  # starts at 0 ang goes up to but NOT including 4
+# print(s[6:7])
+# print(s[6:20])
+# print(s[:2])  # if first # left off, starts at beginning
+# print(s[8:])  # if last number left off, goes to end
+# print(s[:])   # both #s off, prints whole thing
+
+# 'in' as a logical operator
+# fruit = 'banana'
+# print('n' in fruit)
+# print('m' in fruit)
+# print('nan' in fruit)
+# if 'a' in fruit:
+#     print('Found It!')
+
+# greet = 'Hello Bob'
+# zap = greet.lower()
+# print(zap)
+# print(greet)
+# print('Hi There'.lower())
+
+stuff = 'Hello World'
+print(type(stuff))
+print(dir(stuff))
