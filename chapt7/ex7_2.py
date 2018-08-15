a9632cfb5d0ea7067704a4321104dcae484e679b
Chris <c.lamothe25@gmail.com>
2018-08-15 16:55:26 -0400

    Exercise 2!!

--- chapt7/ex7_2.py
+++ chapt7/ex7_2.py
@@ -1,9 +1,14 @@
 fname = input('Enter a file name: ')
 fhand = open(fname)
 count = 0
+total = 0
 for line in fhand:
     line = line.rstrip()
     if line.startswith('X-DSPAM-Confidence'):
         count += 1
-        print(line)
-print(count)
+        spt = line.find('0')
+        ans = line[spt:]
+        fans = float(ans)
+        total = fans + total
+        # print(total)
+print('Average spam confidence:', total / count)
