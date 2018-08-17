34a9330020b3b1ecdb5040740d38d720cccce1ab
Chris <c.lamothe25@gmail.com>
2018-08-16 22:17:05 -0400

    Starting Chapter 8, lists

---
+++ chapt8/tests.py
@@ -0,0 +1,37 @@
+# data structures are a way of organizing data in a computer
+# variables are not collections
+# a collection allows us to put many values in a variable
+
+# for i in [5, 4, 3, 2, 1]:
+#     print(i)
+# print('Blastoff!')
+#
+# friends = ['Luna', 'B', 'Sully']
+# for friend in friends:
+#     print('Happy New Year', friend)
+# print('Done!')
+#
+# z = ['Luna', 'B', 'Sully']
+# for x in z:
+#     print('Happy New Year', x)
+# print('Done!')
+
+# friends = ['Luna', 'B', 'Sully']
+# print(friends[1])
+
+# strings are not mutable
+# fruit = 'Banana'
+# fruit[0] = 'B'
+# print(fruit)
+
+# lists are mutable
+# lotto = [2, 14, 15, 10, 25]
+# lotto[2] = 35
+# print(lotto)
+# print(len(lotto))  # length of list
+
+# range function returns 0 up to/not including the num
+# print(range(4))
+friends = ['Luna', 'B', 'Sully']
+print(len(friends))
+print(range(len(friends)))
