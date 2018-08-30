1d12b4dadc006b6d6a16dcb0b3d0678e4e9b8017
Chris <c.lamothe25@gmail.com>
2018-08-30 07:36:18 -0400

    More regex

--- chapt11/test.py
+++ chapt11/test.py
@@ -1,13 +1,60 @@
 import re
 
 # hand = open('mbox-short.txt')
+# i = 0
 # for line in hand:
 #     line = line.rstrip()
 #     if line.find('From:') >= 0:
+
+#         i += 1
+#         print(i)
 #         print(line)
 
-hand = open('mbox-short.txt')
-for line in hand:
-    line = line.rstrip()
-    if re.search('From: ', line):
-        print(line)
+# hand = open('mbox-short.txt')
+# for line in hand:
+#     line = line.rstrip()
+#     if re.search('From: ', line):
+#         print(line)
+
+# strartswith
+# hand = open('mbox-short.txt')
+# i = 0
+# for line in hand:
+#     line = line.rstrip()
+#     if line.startswith('From:'):
+#         i += 1
+#         print(i)
+#         print(line)
+
+# hand = open('mbox-short.txt')
+# for line in hand:
+#     line = line.rstrip()
+#     if re.search('^From: ', line):
+#         print(line)
+
+# findall
+# x = 'My 2 favorite numbers are 5 and 8'
+# y = re.findall('[0-9]+', x)
+# print(y)
+# y = re.findall('[AEIOU]+', x)
+# print(y)
+
+# greedy matching
+# x = 'From: Using the : Character'
+# y = re.findall('^F.+:', x) # finds the longest match
+# print(y)
+
+# nongreedy matching
+# x = 'From: Using the : Character'
+# y = re.findall('^F.+?:', x) # ? means not greedy (shorter of strings)
+# print(y)
+
+# x = 'From: stephen.marquard@uct.ac.za Sat Jann 4 09:12:15 2018'
+# y = re.findall('\S+@\S+', x)
+# print(y)
+
+# fine tuning
+x = 'From stephen.marquard@uct.ac.za Sat Jann 4 09:12:15 2018'
+# line starts with From; the parens is the text you want extracted
+y = re.findall('^From (\S+@\S+)', x)
+print(y)
