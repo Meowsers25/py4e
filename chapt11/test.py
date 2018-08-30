fae30b539283f018b14bf7f822bc7ee38082aecd
Chris <c.lamothe25@gmail.com>
2018-08-30 17:39:52 -0400

    More regex

--- chapt11/test.py
+++ chapt11/test.py
@@ -53,8 +53,29 @@ import re
 # y = re.findall('\S+@\S+', x)
 # print(y)
 
-# fine tuning
-x = 'From stephen.marquard@uct.ac.za Sat Jann 4 09:12:15 2018'
-# line starts with From; the parens is the text you want extracted
-y = re.findall('^From (\S+@\S+)', x)
+# # fine tuning
+# x = 'From stephen.marquard@uct.ac.za Sat Jann 4 09:12:15 2018'
+# # line starts with From; the parens is the text you want extracted
+# y = re.findall('^From (\S+@\S+)', x)
+# print(y)
+
+# data = 'From stephen.marquard@uct.ac.za Sat Jann 4 09:12:15 2018'
+# atpos = data.find('@')
+# print(atpos)
+# sppos = data.find(' ', atpos)
+# print(sppos)
+# host = data[atpos + 1: sppos]
+# print(host)
+
+# # double split pattern
+# data = 'From stephen.marquard@uct.ac.za Sat Jann 4 09:12:15 2018'
+# words = data.split()
+# email = words[1]
+# pieces = email.split('@')
+# print(pieces[0])
+# # print(words)
+
+# regex way
+data = 'From stephen.marquard@uct.ac.za Sat Jann 4 09:12:15 2018'
+y = re.findall('@([^ ]*)', data)
 print(y)
